a63beaa2772d1074a2db5d6109016560c946d091
Fredy Mauricio Garcia Moná <mauriciogmona@gmail.com>
2023-09-08 16:51:33 -0500

    refact: change name folder

--- src/exception/globlal.exception.ts
+++ src/exception/globlal.exception.ts
@@ -1,6 +1,6 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { Response } from 'express';
-import { GetMessagesStrategy } from './strategy/context-factory/getMessagesStrategy.exception';
+import { GetMessagesStrategy } from './strategy-pattern/context-factory/getMessagesStrategy.exception';
 
 @Catch(HttpException)
 export class GlobalExceptionFilter implements ExceptionFilter {
